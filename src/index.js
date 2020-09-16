5053a6925ef43f84f9ea26a15bd77e245d954f7b
Michael Franz <mfr@trifork.com>
2020-09-16 19:57:15 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import * as THREE from "three";
+import "./style.css";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const distanceFactor = 27;
+var positionIndex = 0;
+
+document.body.style;
 
 const minDistance = 0.25;
 const maxDistance = 200;
@@ -10,19 +16,32 @@ var camera = new THREE.PerspectiveCamera(
   minDistance,
   maxDistance
 );
-camera.position.set(0, 0, 100);
+camera.position.set(0, 0, 50);
 
 var scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 
-var light = new THREE.SpotLight(0xffffff, 1, 120, Math.PI / 3, 0, 1);
-light.position.set(-50, 20, -2.6);
+var light = new THREE.SpotLight(0xffffff, 1, 100, Math.PI / 2, 0.8, 1); // Decay should be used here
+light.position.set(0, 0, 20);
+light.castShadow = true;
 scene.add(light);
 
 var lightTarget = new THREE.Object3D();
 lightTarget.position.set(0, 0, 0);
 scene.add(lightTarget);
 light.target = lightTarget;
+
+var geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// var lightCube = new THREE.Mesh(
+//   new THREE.BoxGeometry(0.1, 0.1, 0.1),
+//   new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// );
+// lightCube.position.set(light.position.x, light.position.y, light.position.z);
+// scene.add(lightCube);
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -31,8 +50,8 @@ document.body.appendChild(renderer.domElement);
 const models = [
   "./assets/gltf/grasshopper.gltf",
   "./assets/gltf/grasshopper.gltf",
-  // "./assets/gltf/grasshopper.gltf",
 ];
+const modelOrigins = [];
 
 var loader = new GLTFLoader();
 
@@ -41,8 +60,23 @@ for (var i = 0; i < models.length; i++) {
   loader.load(
     models[index],
     (gltf) => {
-      console.info("light.target", light.target.position);
-      gltf.scene.position.set(index * 20, 0, 0);
+      var bbox = new THREE.Box3().setFromObject(gltf.scene);
+      const xOffset = (bbox.min.x + bbox.max.x) / 2;
+      const yOffset = (bbox.min.y + bbox.max.y) / 2;
+      const zOffset = (bbox.min.z + bbox.max.z) / 2;
+
+      modelOrigins[index] = {
+        x: index * distanceFactor - xOffset,
+        y: -yOffset,
+        z: -zOffset,
+      };
+
+      gltf.scene.position.set(
+        modelOrigins[index].x,
+        modelOrigins[index].y,
+        modelOrigins[index].z
+      );
+
       scene.add(gltf.scene);
       render();
     },
@@ -57,12 +91,45 @@ const render = () => {
   renderer.render(scene, camera);
 };
 
+// const updateCamera = () => {
+//   camera.position.x = modelOrigins[positionIndex].x;
+//   light.position.x = modelOrigins[positionIndex].x;
+//   lightTarget.position.x = modelOrigins[positionIndex].x;
+//   render();
+// };
+
+var animate = function () {
+  requestAnimationFrame(animate);
+  // const speed = 0.5;
+  // const expectedX = positionIndex * distanceFactor;
+  // if (camera.position.x > expectedX) {
+  //   camera.position.x -= speed;
+  // }
+  // if (camera.position.x < expectedX) {
+  //   camera.position.x += speed;
+  // }
+  // light.position.x = camera.position.x;
+  // lightTarget.position.x = camera.position.x;
+  render();
+};
+animate();
+
 var controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener("change", (e) => {
   console.info("change", camera.position);
   render();
 }); // use if there is no animation loop
+controls.enableKeys = false;
 controls.minDistance = minDistance;
 controls.maxDistance = maxDistance;
 controls.target.set(0, 0, -0.2);
 controls.update();
+
+document.addEventListener("keyup", (e) => {
+  if (e.code === "ArrowRight") {
+    positionIndex = Math.min(models.length - 1, positionIndex + 1);
+  }
+  if (e.code === "ArrowLeft") {
+    positionIndex = Math.max(0, positionIndex - 1);
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
-const path = require('path');
-  const HtmlWebpackPlugin = require('html-webpack-plugin');
-  const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-  module.exports = {
-   mode: 'development',
-    entry: {
-      app: './src/index.js',
-    },
-    plugins: [
-      // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        title: 'Development',
-      }),
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-  };
+  },
+};
